b915acc5ebf529870d1b81d495bd0c0e974e647d
ohkinozomu <nozomunoise@gmail.com>
2022-06-01 22:20:49 +0900

    Reduce data about regions

--- types.go
+++
@@ -1,8 +0,0 @@
-package regions
-
-type Region struct {
-	Name      string  `json:"name"`
-	Flag      string  `json:"flag"`
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
-}
